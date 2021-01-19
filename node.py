5e9bf823b1b683354862dc96cb63097086aa96e9
Edwin George <45304349+toperti@users.noreply.github.com>
2021-01-19 13:55:19 -0800

    Add files via upload

---
+++ node.py
@@ -0,0 +1,19 @@
+# class node for implementing linked list style things 
+
+class Node:
+	def __init__(self, data):
+		self.data = data
+		self.next = None
+
+	def getData(self):
+		return self.data
+
+	def getNext(self):
+		return self.next
+
+	def setData(self, data):
+		self.data = data
+
+	def setNext(self, nxt):
+		self.next = nxt
+
