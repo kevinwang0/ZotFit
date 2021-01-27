d75ad1f3214b411b1cc2c5c6234bf30254a0edb4
bshanehe <hebs@uci.edu>
2021-01-26 16:21:22 -0800

    set up basic homepage

--- 111.py
+++ 111.py
@@ -1,2 +1,10 @@
-def hello():
-    print("hello")
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods = ["GET", "POST"])
+def home():
+	return render_template('home.html')
+
+
+app.run()
