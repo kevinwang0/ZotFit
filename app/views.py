47eafcf48f031e098f2d0886407596c6028992fc
kevinwang0 <kevinw91007@gmail.com>
2021-02-04 02:16:49 -0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'base.html')
+
+def login(request):
+	if request.method == 'POST':
+		form = ImportForm(request.POST)
+		if form.is_valid():
+			return render(request, 'NEW_UNCREATED_HTML.html', {'form':form})
+		else:
+			form = ImportForm()
+	return render(request, 'login.html')
