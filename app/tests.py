4aa713edaf3d397a7a679373bff0b9672e71e15f
kevinwang0 <kevinw91007@gmail.com>
2021-03-03 00:21:31 -0800

    Update tests.py
    test cases for Member helper functions

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from .models import MemberManager, Member
+from django.contrib.auth.models import User
+import datetime
 
 # Create your tests here.
+class MemberHelperTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        user = User.objects.create_user('Chevy Chase', 'chevy@chase.com', 'chevyspassword')
+        Member.objects.createMember(User.objects.get(id=1), height = 80, weight = 180, birth = datetime.date(1998, 10, 19))
+
+    def testAgeScore(self):
+    	member = Member.objects.get(id=1)
+    	print('Age Score: ' + str(member.ageScore()))
+    	self.assertEqual(member.ageScore(), 11)
+
+    def testBMIScore(self):
+        member = Member.objects.get(id=1)
+        print('BMI Score: ' + str(member.bmiScore()))
+        self.assertEqual(member.bmiScore(), 9)
+
+    def testHealthScore(self):
+        member = Member.objects.get(id=1)
+        print('Health Score: ' + str(member.healthScore()))
+        self.assertEqual(member.healthScore(), 20)
